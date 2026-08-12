dee94c7a424891e66cd2c2cbf9a574603890ff4e
taniachavezv <taniachavezv11@gmail.com>
2026-08-11 18:29:57 -0600

    Entrega Práctica 4: Consumo de API para creación de lista de

--- App.js
+++ App.js
@@ -1,20 +1,146 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// CONSUMO DE API Y RENDERIZADO DE IMAGENES
+import { useState, useEffect, Activity } from "react";
+import { StyleSheet, Text, View, FlatList, Image, ActivityIndicator } from "react-native";
 
 export default function App() {
+  // ESTADOS DE LA APLICACION
+  // 'usuarios' guardará los datos descargados de internet (empieza vacío)
+  const [usuarios, setUsuarios] = useState([]);
+  // 'cargando' controla si mostramos la ruedita de espera o la lista. Empieza en true(cargando)
+  const [cargando, setCargando] = useState(true);
+
+  // EL EFECTO DE ARRANQUE (useEffect)
+  // useEffect le dice a la app: "Ejecuta esta función JUSTO DESPUÉS de que la pantalla aparezca por primera vez"
+  // Los corchetes vacíos [] al final significan que solo se ejecutará UNA VEZ al abrir la app
+  useEffect(() => {
+    descargarUsuarios();
+  }, []);
+
+  // LA FUNCIÓN QUE SE CONECTA A INTERNET (Async/Await)
+  const descargarUsuarios = async () => {
+    try {
+      // Hacemos la petición a la base de datos pública (pedimos 10 usuarios)
+      const respuesta = await fetch('https://randomuser.me/api/?results=10'); //fetch permite la comunicacion de la pagina
+      // Convertimos la respuesta a formato JSON
+      const json = await respuesta.json();
+
+      // Guardamos la lista de usuarios en la memoria de la app
+      setUsuarios(json.results);
+      // Apagamos la ruedita de carga
+      setCargando(false);
+    } catch (error) {
+      console.error("Hubo un problema descargando los datos: ", error);
+      setCargando(false);
+    }
+  };
+
+  // PANTALLA DE CARGA (Renderizado condicional)
+  // SI 'cargando' es true, mostramos una ruedita nativa del celular
+  if (cargando) {
+    return (
+      <View style={styles.pantallaCentrada}>
+       <ActivityIndicator size='large' color="#005691" />
+       <Text style={styles.textoCarga}>Descargando perfiles...</Text>
+      </View>
+    );
+  }
+
+  // PANTALLA PRINCIPAL (La lista con datos)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contenedor}>
+      <Text style={styles.tituloPrincipal}>Directorio Global</Text>
+
+      <FlatList
+        data={usuarios}
+        // La API de Random User usa un email único, nos sirve como ID
+        keyExtractor={(item) => item.email}
+        renderItem={({ item }) => (
+          <View style={styles.tarjetaUsuario}>
+
+            {/* Nuevo Componente: Image.
+              Para imágenes de internet, se usa la propiedad 'uri' (Uniform Resource Identifier) */}
+
+            <Image
+              source={{uri: item.picture.large}}
+              style={styles.imagenPerfil}
+              />
+
+            <View style={styles.infoUsuario}>
+              <Text style={styles.nombreUsuario}>
+                {item.name.first} {item.name.last}
+              </Text>
+              <Text style={styles.correoUsuario}>{item.email}</Text>
+              <Text style={styles.paisUsuario}>{item.location.country}</Text>
+            </View>
+            
+          </View>
+        )}
+      />
     </View>
   );
+  
 }
 
+// ESTILOS VISUALES
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f4f7f6',
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  pantallaCentrada: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  textoCarga: {
+    marginTop: 15,
+    fontSize: 16,
+    color: '#666',
+  },
+  tituloPrincipal: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  tarjetaUsuario: {
+    flexDirection: 'row', // Coloca la imagen y los textos uno al lado del otro
+    backgroundColor: '#ffffff',
+    padding: 15,
+    borderRadius: 12,
+    marginBottom: 15,
+    elevation: 3, // Sombras en Android
+    shadowColor: '#000', // Sombras en IOS
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  imagenPerfil:{
+    width: 70,
+    height: 70,
+    borderRadius: 35, // Para que la imagen sea un circulo perfecto (la mitad de width/height)
+    marginRight: 15,
+  },
+  infoUsuario: {
+    flex: 1, // Toma el espacio restante a la derecha de la imagen
     justifyContent: 'center',
   },
+  nombreUsuario: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#222',
+  },
+  correoUsuario: {
+    fontSize: 14,
+    color: '#005691',
+    marginBottom: 4,
+  },
+  paisUsuario: {
+    fontSize: 14,
+    color: '#777',
+  }
 });
